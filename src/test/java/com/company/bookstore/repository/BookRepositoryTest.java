4ace01f2bd21de49f986e27ee48dbca1a9dc07ef
Steven T <32182614+staborda-nmsu@users.noreply.github.com>
2023-03-09 17:25:22 -0700

    Book Repository Test Skeleton

---
+++ src/test/java/com/company/bookstore/repository/BookRepositoryTest.java
@@ -0,0 +1,171 @@
+package com.company.bookstore.repository;
+
+import com.company.bookstore.models.Author;
+import com.company.bookstore.models.Book;
+import com.company.bookstore.models.Publisher;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookRepositoryTest {
+
+    //@MockBean
+    @Autowired
+    BookRepository bookRepo;
+
+    //@MockBean
+    @Autowired
+    AuthorRepository authorRepo;
+
+    //@MockBean
+    @Autowired
+    PublisherRepository publisherRepo;
+
+    @Before
+    public void setUp() throws Exception {
+        bookRepo.deleteAll();
+        authorRepo.deleteAll();
+        publisherRepo.deleteAll();
+    }
+
+    @Test
+    public void addBook() {
+        //Arrange...
+        Book book = new Book();
+        book.setIsbn("999-1-11-123456-0");
+        book.setPublish_date("01/01/1970");
+        book.setTitle("BookTestName1");
+        book.setPrice((float) 16.97);
+
+        Author author = new Author();
+        author.setAuthor_id(1);
+        author.setFirst_name("John");
+        author.setLast_name("Doe");
+        author.setStreet("42 Wallaby Way, Sydney.");
+        author.setCity("Toronto");
+        author.setState("Ohio");
+        author.setPostal_code("43964");
+        author.setPhone("740-123-4567");
+        author.setEmail("JohnDoe@gmail.com");
+
+        Publisher publisher = new Publisher();
+        publisher.setName("John Doe");
+        publisher.setStreet("42 Wallaby Way, Sydney.");
+        publisher.setCity("Toronto");
+        publisher.setState("Ohio");
+        publisher.setPostal_code("43964");
+        publisher.setPhone("740-123-4567");
+        publisher.setEmail("JohnDoe@gmail.com");
+
+        book.setAuthor_id(author.getAuthor_id());
+        book.setPublisher_id(publisher.getPublisher_id());
+
+        //Act...
+        book = bookRepo.save(book);
+        author = authorRepo.save(author);
+        publisher = publisherRepo.save(publisher);
+
+        //Assert...
+        Optional<Book> book1 = bookRepo.findById(book.getBook_id());
+
+        assertEquals(book1.get(), book);
+    }
+
+    @Test
+    public void getAllBooks() {
+        //Arrange...
+
+        //Act...
+        Book book = new Book();
+        book.setIsbn("111-1-11-123456-0");
+        book.setPublish_date("01/02/1970");
+        book.setAuthor_id(1);
+        book.setTitle("BookTestName1");
+        book.setPublisher_id(1);
+        book.setPrice((float) 16.97);
+
+        bookRepo.save(book);
+
+        Book book2 = new Book();
+        book.setIsbn("999-1-11-123456-0");
+        book.setPublish_date("01/03/1970");
+        book.setTitle("BookTestName2");
+        book.setPrice((float) 19.99);
+
+        bookRepo.save(book2);
+
+        List<Book> bookList = bookRepo.findAll();
+
+        //Assert...
+        assertEquals(2, bookList.size());
+    }
+
+    @Test
+    public void updateBook() {
+        //Arrange...
+        Book book = new Book();
+        book.setIsbn("999-1-11-123456-0");
+        book.setPublish_date("01/01/1970");
+        book.setTitle("BookTestName1");
+        book.setPrice((float) 14.97);
+
+        bookRepo.save(book);
+
+        //Act...
+        book.setTitle("UPDATED");
+
+        bookRepo.save(book);
+
+        //Assert...
+        Optional<Book> book1 = bookRepo.findById(book.getBook_id());
+
+        assertEquals(book1.get(), book);
+    }
+
+    @Test
+    public void deleteBook() {
+        //Arrange...
+        Book book = new Book();
+        book.setIsbn("000-1-11-123456-0");
+        book.setPublish_date("01/05/1970");
+        book.setTitle("BookTestName3");
+        book.setPrice((float) 16.97);
+
+        bookRepo.save(book);
+
+        //Act...
+        bookRepo.deleteById(book.getBook_id());
+
+        //Assert...
+        Optional<Book> book1 = bookRepo.findById(book.getBook_id());
+        assertFalse(book1.isPresent());
+    }
+
+    @org.junit.Test
+    public void findBookById() {
+        Book book = new Book();
+        book.setIsbn("555-1-12-123456-0");
+        book.setPublish_date("01/01/1970");
+        book.setTitle("BookTestName1");
+        book.setPrice((float) 12.99);
+
+        bookRepo.save(book);
+
+        //Assert...
+        Optional<Book> book1 = bookRepo.findById(book.getBook_id());
+
+        assertEquals(book1.get(), book);
+    }
+
+}
